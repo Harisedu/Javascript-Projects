0d4a396dac222e22c70ad92bfbad9b4e4c05bb4e
Haris Ali <93787994+Harisedu@users.noreply.github.com>
2022-01-17 09:27:10 +0500

    Update index.js

--- Counter/index.js
+++ Counter/index.js
@@ -1,9 +1,13 @@
-let countEl = document.getElementById("count-el") //pass in arguments
-console.log(countEl)
-
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment(){
-    count = count + 1
+function increment() {
+    count += 1
     countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
 }
